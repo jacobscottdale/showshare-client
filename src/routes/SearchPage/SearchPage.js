252cb0ee6057344f65ffc740c85a9f919536d8ec
Jacob Dale <jacobscottdale@gmail.com>
2020-11-17 14:57:17 -0500

    SearchPage created. Fetch API nonfuncational at the moment.

--- src/routes/SearchPage/SearchPage.js
+++ src/routes/SearchPage/SearchPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from 'components/SearchBar/SearchBar.js';
 import ShowList from 'components/ShowList/ShowList.js';
+import config from 'config';
 import { tvShows } from 'store';
 
 class SearchPage extends Component {
@@ -9,12 +10,31 @@ class SearchPage extends Component {
   };
 
   handleSearch = searchTerm => {
-    console.log('calling');
+    /*console.log('calling');
     const showsResults = tvShows.filter(show => show.title.toLowerCase().includes(searchTerm.toLowerCase())).map(show => show.ids.trakt);
     console.log(showsResults);
     this.setState({
       showsResults: showsResults
-    });
+    });*/
+    fetch(`${config.TRAKT_API_URL}/search/show?query=${searchTerm}`, {
+      headers: {
+        'Content-type': 'application/json',
+        'trakt-api-key': config.TRAKT_API_KEY,
+        'trakt-api-version': '2'
+      }
+    })
+    .then(res => {
+      if (!res.ok)
+        throw new Error(res.status)
+      return res.json()
+    })
+    .then(showsResults => {
+      console.log(showsResults)
+      this.setState({
+        showsResults: showsResults
+      })
+    })
+    .catch(err => console.log(err))
   };
 
   render() {
